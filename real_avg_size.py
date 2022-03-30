9b6c73c58f808bb59358799f9c6f757cbc3397f5
usr <usr@mail.com>
2022-03-30 18:44:29 +0200

    bug fix,  standalone exec

--- real_avg_size.py
+++ real_avg_size.py
@@ -11,6 +11,7 @@ import datetime
 from tqdm import tqdm
 
 
+"""Script for getting the average set size per alpha value in real data experiments"""
 
 results_root = f"{conf.ROOT_DIR}/results_real"
 if not os.path.exists(results_root):
@@ -20,8 +21,8 @@ results_root+=f"/calibrationSet{conf.cal_split}"
 if not os.path.exists(results_root):
     os.mkdir(results_root)
 
-
-
+# Compute average set size per alpha for all human and model combinations 
+# given the calibration and estimation split
 for model_name in conf.model_names:
     for run in tqdm(range(conf.n_runs_per_split)):
 
@@ -29,43 +30,32 @@ for model_name in conf.model_names:
         if not os.path.exists(res_dir):
             os.mkdir(res_dir)
       
-       
+        # Read and split dataset
         X_test, X_cal, X_est, y_test, y_cal, y_est = utils.make_dataset_real(run)
                 
-
+        # Human expert
         conf.accuracy = None
         human = ExpertReal(conf)
-
+        
+        # Create model
         model = ModelReal(model_name)
 
         conf_pred = StandardCPgpu(X_cal, y_cal,X_est, y_est, model, conf.delta)
-        with open(f"{res_dir}/alphas1",'rb') as f1:
-            alphas =  pickle.load(f1)
+
+        # Get alpha values
+        alphas_dir = f"{res_dir}/alphas1"
+        if os.path.exists(alphas_dir):
+            with open(alphas_dir,'rb') as f1:
+                alphas =  pickle.load(f1)
+        else:
+            alphas = conf_pred.find_all_alpha_values()
+            with open(alphas_dir,'wb') as f1:
+                pickle.dump(alphas, f1 ,pickle.HIGHEST_PROTOCOL)
+    
+
         size_t = conf_pred.size_given_test_set_per_a(X_test,y_test, human.w_matrix,alphas)
         size = size_t.detach().cpu().numpy()
         with open(f"{res_dir}/set_size_test",'wb') as f2:
             pickle.dump(size, f2 ,pickle.HIGHEST_PROTOCOL)            
                    
-                   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                
--- synthetic_avg_size.py
+++ synthetic_avg_size.py
@@ -10,6 +10,7 @@ import sys
 import datetime
 from tqdm import tqdm
 
+"""Script for getting the average set size per alpha value in synthetic experiments"""
 
 results_root = f"{conf.ROOT_DIR}/results_synthetic"
 if not os.path.exists(results_root):
@@ -19,6 +20,8 @@ results_root+=f"/{conf.n_labels}labels_calibrationSet{conf.cal_split}"
 if not os.path.exists(results_root):
     os.mkdir(results_root)
 
+# Compute average set size per alpha for all human and model combinations 
+# given the number of labels, and calibration and estimation split
 for human_accuracy in conf.accuracies:
     for machine_accuracy in conf.accuracies:
         for run in tqdm(range(conf.n_runs_per_split)):
@@ -27,27 +30,33 @@ for human_accuracy in conf.accuracies:
             if not os.path.exists(res_dir):
                 os.mkdir(res_dir)
             
+            # Generate dataset
             X_train, X_test,X_cal,X_est, y_train, y_test,y_cal, y_est  = utils.make_dataset(run, machine_accuracy)
-
+            
+            # Human expert
             conf.accuracy = human_accuracy
             human = ExpertSynthetic(conf)
 
+            # Create model
             model = ModelSynthetic()
             model.train(X_train,y_train)
             print(model.test(X_test, y_test))
 
             conf_pred = StandardCPgpu(X_cal, y_cal,X_est, y_est, model, conf.delta)
             print(res_dir)
-            with open(f"{res_dir}/alphas1",'rb') as f:
-                alphas = pickle.load(f)
             
-            
+            # Get alpha values
+            alphas_dir = f"{res_dir}/alphas1"
+            if os.path.exists(alphas_dir):
+                with open(alphas_dir,'rb') as f:
+                    alphas = pickle.load(f)
+            else:
+                alphas = conf_pred.find_all_alpha_values()
+                with open(alphas_dir,'wb') as f1:
+                    pickle.dump(alphas, f1 ,pickle.HIGHEST_PROTOCOL)
 
+            
             size_t = conf_pred.size_given_test_set_per_a(X_test,y_test, human.w_matrix,alphas)
             size = size_t.detach().cpu().numpy()
             with open(f"{res_dir}/set_size_test",'wb') as f2:
                 pickle.dump(size, f2 ,pickle.HIGHEST_PROTOCOL)
-
-
-            
-            print("Done")
